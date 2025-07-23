6b94c150ee7b8dd5d0ec4ff385d9209a3d7e51b0
vpeopleonatank <vpeopleonatank@gmail.com>
2025-07-23 18:57:41 +0700

    update

--- lua/kickstart/plugins/diffview-nvim.lua
+++ lua/kickstart/plugins/diffview-nvim.lua
@@ -2,11 +2,40 @@ return {
   'sindrets/diffview.nvim',
   lazy = false,
   keys = {
-    -- {
-    --   '<leader>ng',
-    --   mode = { 'n', 'v' },
-    --   '<cmd>Neogit<cr>',
-    --   desc = 'Open neogit',
-    -- },
+    {
+      '<leader>dv',
+      '<cmd>DiffviewOpen<cr>',
+      desc = '[D]iff[v]iew open',
+    },
+    {
+      '<leader>dc',
+      '<cmd>DiffviewClose<cr>',
+      desc = '[D]iff[c]lose',
+    },
+    {
+      '<leader>df',
+      '<cmd>DiffviewFileHistory<cr>',
+      desc = '[D]iff [f]ile history',
+    },
+    {
+      '<leader>dr',
+      '<cmd>DiffviewRefresh<cr>',
+      desc = '[D]iff [r]efresh',
+    },
+    {
+      '<leader>do',
+      '<cmd>DiffviewFileHistory %<cr>',
+      desc = '[D]iff [o]pen current file',
+    },
+    {
+      '<leader>dt',
+      '<cmd>DiffviewToggleFiles<cr>',
+      desc = '[D]iff [t]oggle files',
+    },
+    {
+      '<leader>ds',
+      '<cmd>DiffviewOpen HEAD~1<cr>',
+      desc = '[D]iff [s]taged changes',
+    },
   },
 }
